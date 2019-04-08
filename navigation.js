82490eeebece1f51d609d59bcddc8317745219de
Cristian Ramos <alu0100904406@ull.edu.es>
2019-04-08 20:00:59 +0100

    Añadido funcion de mover el mapa con el raton, quitado path que se esconda al llegar al objetivo

--- navigation.js
+++ navigation.js
@@ -2,8 +2,14 @@ class Navigation {
 
   constructor() {
 
-    this.scale_width = 400;
-    this.scale_heigth = 400;
+    this.position;
+    this.goal;
+
+    this.actual_path_message = null;
+
+    this.scale_width;
+    this.scale_heigth;
+
     this.ros = new ROSLIB.Ros({
       url : 'ws://localhost:9090'//Conexion a rosbridge
     });
@@ -33,6 +39,7 @@ class Navigation {
     this.path = new ROS2D.PathShape({ strokeColor: createjs.Graphics.getRGB(239, 48, 14, 0.66)});
     this.path_state = false;
     this.switch_path();
+
   }
 
   _initialize_topics(){
@@ -65,17 +72,25 @@ class Navigation {
         name        : '/move_base/cancel',
         messageType : 'actionlib_msgs/GoalID'
     });
+
+    this.statusTopic = new ROSLIB.Topic({
+        ros         : this.ros,
+        name        : '/move_base/status',
+        messageType : 'actionlib_msgs/GoalStatusArray'
+    })
   }
 
   _set_marker_on_map(){
     this.initScaleSet = false;
     this.gridClient.rootObject.addChild(this.robotMarker);
     this.poseTopic.subscribe(function(message) {
-      console.log("pose");
-      //console.log(message.pose.pose.position.x, message.pose.pose.position.y);
+
+      this.position = message.pose.pose;
+
       // Orientate the marker based on the robot's pose.
-      this.robotMarker.x = message.pose.pose.position.x;
-      this.robotMarker.y = -message.pose.pose.position.y;
+      this.robotMarker.x = this.position.position.x;
+      this.robotMarker.y = -this.position.position.y;
+
       if (!this.initScaleSet) {
         this.robotMarker.scaleX = 1.0 / this.viewer2D.scene.scaleX;
         this.robotMarker.scaleY = 1.0 / this.viewer2D.scene.scaleY;
@@ -86,22 +101,24 @@ class Navigation {
     }.bind(this));
   }
 
-  _set_pose_controller(){
-    this.viewer2D.scene.addEventListener('stagemouseup', function(event) {
+  _set_goal_controller(){
+    this.viewer2D.scene.on('dblclick', function(event) {
       if (this.viewer2D.scene.mouseInBounds === true) {
-				var pos = this.viewer2D.scene.globalToRos(event.stageX, event.stageY);
-        var goal = new ROSLIB.Message({
+				var goal_pose = this.viewer2D.scene.globalToRos(event.stageX, event.stageY);
+        var goal_message = new ROSLIB.Message({
           header:
           {
             frame_id: "map"
           },
           pose:
           {
-            position: {x: pos.x, y: pos.y, z: 0.0},
-            orientation: {w: 1.0}
+            position: {x: goal_pose.x, y: goal_pose.y, z: 0.0},
+            orientation: {x:0, y:0, z:0, w: 1.0}
           }
         });
-        this.goalTopic.publish(goal);
+
+        this.goal = goal_message.pose
+        this.goalTopic.publish(goal_message);
       }
     }.bind(this));
   }
@@ -118,29 +135,28 @@ class Navigation {
       rootObject : this.viewer2D.scene
     });
 
-
     this.gridClient.on('change', function() {
       this.scale_width = this.gridClient.currentGrid.width;
       this.scale_heigth = this.gridClient.currentGrid.height;
-      //this.viewer2D.scaleToDimensions(this.gridClient.currentGrid.width, this.gridClient.currentGrid.height);
       this.zoom(0);
       this.viewer2D.shift(this.gridClient.currentGrid.pose.position.x, this.gridClient.currentGrid.pose.position.y);
-      //$(".lds-dual-ring").remove()
+
       $("canvas").css("visibility", "visible");
-      //console.log(viewer2D.scene.children);
-      //viewer2D.scene.children[0].image.getContext('2d').drawImage(viewer2D.scene.children[0].image, 0, 0, 1000, 1000);
-      this._set_marker_on_map();
-      this._set_pose_controller();
+
       this.gridClient.rootObject.addChild(this.path);
-      //this.gridClient.getMatrix();
+      this._set_marker_on_map();
+      this._set_goal_controller();
+
     }.bind(this));
   }
 
   switch_path(){
-    if(!this.path_state){
+    if(this.path_state === false){
       this.path_state=true;
+      this.path.setPath(this.actual_path_message);
       this.pathTopic.subscribe(function(message) {
-          this.path.setPath(message);
+        this.actual_path_message = message;
+        this.path.setPath(this.actual_path_message);
       }.bind(this));
     }
     else {
@@ -158,12 +174,21 @@ class Navigation {
 
   view_speed(id){
     this.velTopic.subscribe(function(message){
-      id.text("Velocidad: " + message.linear.x.toFixed(2) + " m/s");
+      id.text("Speed: " + message.linear.x.toFixed(2) + " m/s");
     })
   }
 
   stop(){
-    this.path.setPath(null);
+    this.clear_path()
     this.stopTopic.publish({});
+  }
+
+  clear_path(){
+    this.actual_path_message = null;
+    this.path.setPath(null);
+  }
+
+  shift_map(x,y){
+    this.viewer2D.shift(x,y);
   }
 }

fb706074a2ec1fd274bc0ae3e653c635efd19577
Cristian Ramos <alu0100904406@ull.edu.es>
2019-05-26 18:38:56 +0100

    Añadiendo grafico batería

--- public/carer/js/graphs.js
+++ public/carer/js/graphs.js
@@ -2,17 +2,12 @@ rosHeartbeat = new ROSLIB.Ros({
     url : 'ws://127.0.0.1:9090'//Conexion a rosbridge
 });
 
+//TERMOMETRO
 var temperature = new ROSLIB.Topic({
     ros         : rosHeartbeat,
-    name        : '/temperature',
+    name        : '/temperature_avg',
     messageType : 'std_msgs/Float64'
 });
-
-var battery = new ROSLIB.Topic({
-    ros         : rosHeartbeat,
-    name        : '',
-    messageType : 'std_msgs/Float64'
-})
 
 var svg = d3.select('body').append('svg').attr("width", 40).attr("height",250);
 
@@ -28,64 +23,32 @@ temperature.subscribe(function(msg){
                 .attr("x", 10)
                 .attr("y", function(temperatura){ return 250 - temperatura - 15})
                 .attr("width", 5)
-                .attr("height", function(temperatura){ return temperatura})
+                .attr("height", function(temperatura){ 
+                    if (temperatura < 0) {//Arreglar para temperaturas negativas!Esto no vale!
+                        return 0;
+                    }
+                    else {
+                        return temperatura;
+                    }
+                })
                 .attr("style", "fill:red;")
                 .attr('class', 'bar');
 
     bar = svg.selectAll('.bar').data([temperatura*10/2]);
     
-    bar.attr('height',  function(t){ return t}).attr('width', 5).attr("y", function(t){ return 250 - t -15});   
+    bar.attr('height',  function(temperatura){ 
+        if (temperatura < 0) {//Arreglar para temperaturas negativas!Esto no vale!
+            return 0;
+        }
+        else {
+            return temperatura;
+        }
+    })
+    .attr('width', 5).attr("y", function(t){ return 250 - t -15});   
 });
 
-/*d3.select();//El primer elemento del dom que cumpla el criterio
-d3.selectAll();//Todos
-d3.select('h1')
-.style('color', 'red')
-.attr('class', 'heading')
-.text('Map')
-.append('p').text('hola');*/
+//ESTO CON JQUERY Y LAS GRAFICAS CON GOOGLE CHARTS
+var svg_battery = d3.select('body').append('svg').attr("width", 50).attr("height",100);
 
-/*var dataset1 = [1,2,3,4,5];
-d3.select('body').
-selectAll('p').
-data(dataset1).
-enter().
-append('p').
-text(function(data) { return data})*/
-
-/*var datasetbars = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-
-var svgWidth = 500;
-var svgHeight = 300;
-var barPadding = 5;
-var barWidth = svgWidth / datasetbars.length
-
-
-
-var barChart = svg.selectAll('rect')
-                    .data(datasetbars)
-                    .enter()
-                    .append('rect')
-                    .attr('y', function(d) { return svgHeight - d})
-                    .attr('height', function(d){ return d})
-                    .attr('width', barWidth - barPadding)
-                    .attr('transform', function(d, i){ 
-                        var translate = [barWidth * i, 0];
-                        return "translate(" + translate + ")";
-                    });*/
-
-//scalelinear
-/*
-   axisTop
-   axisRight
-   axisBottom
-   axisLeft
-*/
-
-//Transform
-//pie()
-//arc()
-//scaletime
-//rangeround
-//line()
-//extend()
+svg_battery.append('rect').attr('y', 100-75).attr('height',75).attr('width', 50).attr('fill','green');
+svg_battery.append('rect').attr('y', 0).attr('height',100).attr('width', 50).attr('style', 'stroke-width:8;stroke:rgb(0,0,0)').attr('fill-opacity','0');
